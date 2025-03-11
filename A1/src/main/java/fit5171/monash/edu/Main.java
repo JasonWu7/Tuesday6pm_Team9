b450262a05b8cf16e5af56979a34eef3fd5a5ddf
Dongzheng Wu <dwuu0018@student.monash.edu>
2025-03-11 21:25:07 +1100

    initial commit

---
+++ A1/src/main/java/fit5171/monash/edu/Main.java
@@ -0,0 +1,12 @@
+package fit5171.monash.edu;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        TicketSystem ticketSystem = new TicketSystem();
+        BuyTicket buyTicket = new BuyTicket();
+    }
+}
