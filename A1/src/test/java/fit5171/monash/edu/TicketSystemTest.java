aa81b685b00ebfe39c7ddddf8191fade328c3387
haobo <ghaobo171@gmail.com>
2025-03-14 18:45:07 +1100

    add junit dependency and create TicketSystemTest class

---
+++ A1/src/test/java/fit5171/monash/edu/TicketSystemTest.java
@@ -0,0 +1,29 @@
+package fit5171.monash.edu;
+
+import static org.junit.jupiter.api.Assertions.*;
+class TicketSystemTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void showTicket() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void chooseTicket() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void buyTicket() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testBuyTicket() {
+    }
+}
